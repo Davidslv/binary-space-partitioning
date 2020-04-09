c1aa4cbdb91bfdc7a15e299befb5f09ff66c57ee
David Silva <davidslv@gmail.com>
2020-04-09 08:28:14 +0100

    Update Dijkstra output

--- lib/vanilla/dijkstra.rb
+++ lib/vanilla/dijkstra.rb
@@ -1,14 +1,21 @@
+require_relative 'output/png'
 require_relative 'output/terminal'
 require_relative 'cell_distance'
 require_relative 'distance_grid'
 require_relative 'binary_tree'
 
-grid = Vanilla::DistanceGrid.new(rows: 5, columns: 5)
-Vanilla::BinaryTree.on(grid)
+2.times do
+  grid = Vanilla::DistanceGrid.new(rows: 10, columns: 10)
+  Vanilla::BinaryTree.on(grid)
 
-start = grid[0, 0]
-distances = start.distances
+  start = grid[0, 0]
+  distances = start.distances
 
-grid.distances = distances
+  puts "path from northwest corner to southwest corner:"
+  grid.distances = distances.path_to(grid[grid.rows - 1, 0])
 
-puts Vanilla::Output::Terminal.new(grid)
+  puts Vanilla::Output::Terminal.new(grid)
+  Vanilla::Output::Png.new(grid).to_png if false # turn off
+
+  sleep 0.4
+end
